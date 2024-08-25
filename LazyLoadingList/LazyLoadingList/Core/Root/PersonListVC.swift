4eac365c7e737a5227828c5ce9acddce074d8b4d
Gerard <gersapinads@gmail.com>
2024-08-25 13:20:34 +0800

    convert PersonVCList to rxswift

--- LazyLoadingList/LazyLoadingList/Core/Root/PersonListVC.swift
+++ LazyLoadingList/LazyLoadingList/Core/Root/PersonListVC.swift
@@ -7,65 +7,95 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PersonListVC: UIViewController {
     
     var coordinator : AppCoordinator?
     private var viewModel = PersonListViewModel()
+    private let disposeBag = DisposeBag()
+    private let refreshControl = UIRefreshControl()
     
-    private lazy var contentView: PersonListView = {
-        let view = PersonListView()
-        view.delegate = self
-        return view
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.register(PersonListViewCell.self, forCellReuseIdentifier: "cell")
+        table.rx.setDelegate(self).disposed(by: disposeBag)
+        refreshControl.addTarget(self, action: #selector(didRefreshTable), for: .valueChanged)
+        table.refreshControl = refreshControl
+        table.separatorStyle = .none
+        return table
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
         title = "Persons List"
         let backButton = UIBarButtonItem()
         backButton.title = title
         navigationItem.backBarButtonItem = backButton
         
-        viewModel.reloadData = {
-            DispatchQueue.main.async { [weak self] in
-                if let persons = self?.viewModel.persons {
-                    self?.contentView.configPersonList(data: persons)
-                }
-            }
-        }
-        viewModel.fetchPersons()
+        setupUI()
+        bindViewModel()
         viewModel.fetchPersonsRx()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
         
-        view = contentView
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func bindViewModel() {
+        viewModel.personsRx
+            .bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: PersonListViewCell.self)) { index, person, cell in
+                cell.configCell(data: person)
+        }.disposed(by: disposeBag)
+        
+        tableView.rx.itemSelected
+            .subscribe { [weak self] indexPath in
+                self?.handleRowSelection(at: indexPath)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func handleRowSelection(at indexPath: IndexPath) {
+        coordinator?.routeToPersonDetails(data: viewModel.personsRx.value[indexPath.row])
     }
 
 }
 
-//MARK: delegate
-extension PersonListVC: PersonListViewDelegate {
-    func willLoadMorePersons(_ view: PersonListView, completion: @escaping () -> Void) {
-        viewModel.loadMorePersons {
-            completion()
+//MARK: action
+extension PersonListVC {
+    @objc func didRefreshTable(){
+        viewModel.refreshPersonsRx() { [weak self] in
+            self?.refreshControl.endRefreshing()
         }
     }
+}
+
+//MARK: delegate
+extension PersonListVC: UITableViewDelegate, UIScrollViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.height / 10
+    }
     
-    func didRefreshTable(_ view: PersonListView) {
-        viewModel.refreshPersons()
-        viewModel.reloadData = {
-            DispatchQueue.main.async { [weak self] in
-                if let persons = self?.viewModel.persons {
-                    self?.contentView.configPersonList(data: persons)
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let tableHeight = tableView.bounds.size.height
+        let contentHeight = tableView.contentSize.height
+        let contentOffsetY = scrollView.contentOffset.y
+        
+        if(contentOffsetY+tableHeight > contentHeight && contentHeight > 0 && contentOffsetY > 0){
+            if !viewModel.isFetchingMoreData {
+                viewModel.isFetchingMoreData = true
+                
+                viewModel.loadMorePersonsRx { [weak self] in
+                    self?.viewModel.isFetchingMoreData = false
                 }
             }
         }
-        
-        contentView.refreshControl.endRefreshing()
-    }
-    
-    func didTapPerson(_ view: PersonListView, data: Person) {
-        coordinator?.routeToPersonDetails(data: data)
     }
 }
 
